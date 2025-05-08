955933b3eb2289c2c7731f2d8ef2469ccc9796db
LeoBC24 <s2401553@edu.bc.fi>
2025-05-08 10:40:40 +0300

    deployed v2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/part1/', // 👈 use your GitHub repo name here
+  base: '/React_part1/', // 👈 use your GitHub repo name here
   plugins: [react()],
 })
